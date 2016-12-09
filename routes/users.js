e89e8ae6d913ad3decc1a41e7136b171cebd429b
eryueweb <eryue0123@163.com>
2016-12-09 17:55:57 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var	$userOperate = require('../data/sql/userOperate');
+var $userOperate = require('../data/sql/userOperate');
 /* GET users listing. */
 router.get('/add', function(req, res, next) {
 	$userOperate.add(req,res,next)
